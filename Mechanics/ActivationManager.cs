db23775034d813894a8e2af7941b0c3e3a0868e8
Trident Xuan Tew <39085693+trident4896@users.noreply.github.com>
2019-07-31 23:00:18 +0800

    Add files via upload

---
+++ Mechanics/ActivationManager.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ActivationManager : MonoBehaviour
+{
+    public GameObject[] stage1_Puzzle;
+    public GameObject[] stage2_Puzzle;
+    public GameObject[] stage3_Puzzle;
+
+    public GameObject[] stage1_clue;
+    public GameObject[] stage2_clue;
+    public GameObject[] stage3_clue;
+
+    public GameObject[] stage1_prop;
+    public GameObject[] stage2_prop;
+    public GameObject[] stage3_prop;
+
+    public GameObject[] stage1_OrignalNode;
+    public GameObject[] stage2_OriginalNode;
+    public GameObject[] stage3_OriginalNode;
+
+    public GameObject[] stage1_EndNode;
+    public GameObject[] stage2_EndNode;
+    public GameObject[] stage3_EndNode;
+
+    public static int stage1_puzzle_count= 0;
+    public static int stage2_puzzle_count = 0;
+
+    void Start()
+    {
+        stage1_activation();
+    }
+
+    void Update()
+    {
+        //if stage 1 is done, activate stage 2 objects
+       if (stage1_puzzle_count == 3 && TriggerManager.Stage1_ProgressCheck)
+        {
+            stage2_activation();
+        }
+    }
+
+    //stage 2 objects 
+    void stage2_activation()
+    {
+        stage1_Puzzle[0].GetComponent<BoxCollider>().enabled = false;
+        stage1_Puzzle[1].GetComponent<BoxCollider>().enabled = false;
+        stage1_Puzzle[2].GetComponent<BoxCollider>().enabled = false;
+
+        stage2_Puzzle[0].GetComponent<BoxCollider>().enabled = true;
+        stage2_Puzzle[1].GetComponent<BoxCollider>().enabled = true;
+        stage2_Puzzle[2].GetComponent<BoxCollider>().enabled = true;
+
+        stage1_clue[0].GetComponent<BoxCollider>().enabled = false;
+        stage1_clue[1].GetComponent<BoxCollider>().enabled = false;
+        stage1_clue[2].GetComponent<BoxCollider>().enabled = false;
+
+        stage2_clue[0].GetComponent<BoxCollider>().enabled = true;
+        stage2_clue[1].GetComponent<BoxCollider>().enabled = true;
+        stage2_clue[2].GetComponent<BoxCollider>().enabled = true;
+
+        stage1_prop[0].GetComponent<BoxCollider>().enabled = false;
+        stage1_prop[1].GetComponent<BoxCollider>().enabled = false;
+
+        stage2_prop[0].GetComponent<BoxCollider>().enabled = true;
+        stage2_prop[1].GetComponent<BoxCollider>().enabled = true;
+    }
+
+    //stage 1 objects
+    void stage1_activation()
+    {
+        stage1_Puzzle[0].GetComponent<BoxCollider>().enabled = true;
+        stage1_Puzzle[1].GetComponent<BoxCollider>().enabled = true;
+        stage1_Puzzle[2].GetComponent<BoxCollider>().enabled = true;
+
+        stage2_Puzzle[0].GetComponent<BoxCollider>().enabled = false;
+        stage2_Puzzle[1].GetComponent<BoxCollider>().enabled = false;
+        stage2_Puzzle[2].GetComponent<BoxCollider>().enabled = false;
+
+        stage1_clue[0].GetComponent<BoxCollider>().enabled = true;
+        stage1_clue[1].GetComponent<BoxCollider>().enabled = true;
+        stage1_clue[2].GetComponent<BoxCollider>().enabled = true;
+
+        stage2_clue[0].GetComponent<BoxCollider>().enabled = false;
+        stage2_clue[1].GetComponent<BoxCollider>().enabled = false;
+        stage2_clue[2].GetComponent<BoxCollider>().enabled = false;
+
+        stage1_prop[0].GetComponent<BoxCollider>().enabled = true;
+        stage1_prop[1].GetComponent<BoxCollider>().enabled = true;
+
+        stage2_prop[0].GetComponent<BoxCollider>().enabled = false;
+        stage2_prop[1].GetComponent<BoxCollider>().enabled = false;
+    }
+}
---
+++ Mechanics/objectClass.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[System.Serializable]
+    public class objectClass: MonoBehaviour
+    {
+         public class Puzzle
+        {
+            public string puzzle_name;
+            public bool puzz_activation;
+
+        public Puzzle(string p_nam, bool p_acti)
+        {
+            puzzle_name = p_nam;
+            puzz_activation = p_acti;
+        }
+    }
+
+    public Puzzle myPuzzle1 = new Puzzle("Puzzle 01", false);
+
+    public class Clue
+    {
+        public string clue_name;
+        public bool clue_activation;
+
+        public Clue(string c_nam, bool c_acti)
+        {
+            clue_name = c_nam;
+            clue_activation = c_acti;
+        }
+    }
+
+    public Clue myClue1 = new Clue("Clue 01", false);
+
+    public class Prop
+    {
+        public string prop_name;
+        public bool prop_activation;
+        public BoxCollider _col;
+
+        public Prop(string pr_nam, bool pr_acti)
+        {
+            prop_name = pr_nam;
+            prop_activation = pr_acti;
+        }
+    }
+
+    public Prop myProp1 = new Prop ("Prop 01", false);
+
+    // Start is called before the first frame update
+    void Start()
+        {
+   
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
