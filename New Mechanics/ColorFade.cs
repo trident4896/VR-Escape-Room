1254dcc548336e773ed49205236baf96fed30b1a
Trident Xuan Tew <39085693+trident4896@users.noreply.github.com>
2019-08-19 20:57:37 +0800

    Add files via upload

---
+++ New Mechanics/ColorFade.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ColorFade : MonoBehaviour
+{
+
+    [SerializeField] private bool saveColorOnce = false;
+    [SerializeField] private Transform thisObject;
+
+    [SerializeField] private Color startColor;
+    [SerializeField] private Color EndColor;
+
+    [SerializeField] private float lerpFadeTime = 0f;
+    private float fadeDuration = 0.8f;
+
+    private void Start()
+    {
+        startColor = this.transform.GetComponent<Renderer>().material.color;
+        thisObject = this.transform;
+    }
+
+    public void FadeCheck(bool fadingStatus)
+    {
+        if(fadingStatus == true)
+        {
+            FadeIn();
+        }
+        else
+        {
+            FadeOut();
+        }
+
+    }
+
+    private void FadeIn()
+    { 
+            lerpFadeTime += Time.deltaTime / fadeDuration;
+
+            thisObject.GetComponent<Renderer>().material.color = Color.Lerp(startColor, EndColor, lerpFadeTime);
+
+            if(lerpFadeTime >= 1f)
+            {
+                lerpFadeTime = 1f;
+            }
+    }
+
+    private void FadeOut()
+    {
+            lerpFadeTime -= Time.deltaTime / fadeDuration;
+
+            thisObject.GetComponent<Renderer>().material.color = Color.Lerp(startColor, EndColor, lerpFadeTime);
+
+            if (lerpFadeTime <= 0f)
+            {
+                lerpFadeTime = 0f;
+            }
+    }
+}
---
+++ New Mechanics/TriggerCheck.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerCheck : MonoBehaviour
+{
+   
+    [SerializeField] private GameObject itemCollide;
+    [SerializeField] private GameObject nodeCollide;
+    public static int progressCounter = 0;
+
+    public ProgressionCheck saveProgressCounter;
+    public GameObject saveProgressCounterObject;
+
+    public GameObject targetPuzzle;
+    private ParticleSystem sparkEffect;
+
+    public void Awake()
+    {
+        saveProgressCounter = saveProgressCounterObject.GetComponent<ProgressionCheck>();
+        sparkEffect = GetComponent<ParticleSystem>();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        itemCollide = null;
+        itemCollide = other.transform.gameObject;
+
+        if(GameObject.Equals(targetPuzzle, itemCollide))
+        {
+            sparkEffect.Play();
+            other.enabled = false;
+            progressCounter += 1;
+        }
+
+        saveProgressCounter.saveTheProgressCounter = progressCounter;
+    }
+}
